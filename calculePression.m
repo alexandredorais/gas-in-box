e9e48e77804abc6fdd9d8fb1f6eb2483d14c0be3
alexandredorais <alex.dorais.7@gmail.com>
2019-03-27 12:45:05 -0400

    delete les fichiers qui servent à rien pour la question B

--- calculePression.m
+++
@@ -1,8 +0,0 @@
-function[P_exp, P_th]=calculePression(deltap_axe,S,deltat, N, T, V)
-    % pression gaz parfaits
-    kB = 1.3806e-23;
-    P_th = N*kB*T/V;
-    % pression experimentale
-    VecP=deltap_axe./(deltat.*S);
-    P_exp=sum(VecP);
-end
--- libreParcoursMoy.m
+++
@@ -1,9 +0,0 @@
-function[lambdaexp, lambdatheo]=libreParcoursMoy(P,v_moy,moydeltat)
-    % libre parcours moyen experimental
-    lambdaexp=v_moy.*moydeltat;
-    % libre parcours moyen theorique
-    kB = 1.3806e-23;
-    T=300;
-    sigmaeff=0.27*(1e-9)^2;
-    lambdatheo=kB.*T./(P.*sigmaeff);
-end
